48b828509dcb0523eb6dea6592e4ba7762b70133
shravanitechcode <hangargekarshravani40@gmail.com>
2025-03-15 08:55:47 +0530

    short hand operator

---
+++ program 8.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+void main()
+{
+
+ int a=1000;
+     a+=500;
+
+printf("%d\n",a);
+
+
+int b=1000;
+    b*=500;
+printf("%d\n",b);
+
+int c=1000;
+    c-=500;
+printf("%d\n",c);
+
+int d=1000;
+    d/=500;
+printf("%d\n",d);
+
+int e=1000;
+    e%=500;
+printf("%d",e);
+
+int age;
+printf("enter your age");
+scanf("%d",&age);
+
+age+=3;
+
+printf("after 3year age=%d",age);
+
+
+
+
+
+
+
+
+
+
+
+
+}
